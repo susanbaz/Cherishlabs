eb1a6d903293a7fef7179e0568a47fc58a7023da
Susan Brooks <susantans@icloud.com>
2023-09-26 21:56:34 -0700

    6th commit

--- joinrebate/server/db/client.js
+++ joinrebate/server/db/client.js
@@ -6,7 +6,7 @@ const { Client } = require('pg');
 const dbName = 'cherishlab';
 const dbUser = 'cherishlab';       // User role name
 const dbPassword = 'CRxaGJkgGHGYEoow'; // User password (if applicable)
-const connectionString = process.env.DATABASE_URL || `postgres:cherishlab-owsf4-postgresql.external.kinsta.app/${dbName}`;
+const connectionString = postgres://cherishlab:CRxaGJkgGHGYEoow@cherishlab-owsf4-postgresql.external.kinsta.app:31885/cherishlab ;
 
 
 
